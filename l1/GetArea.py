02c75af8426b429f9e0bbe99d58a0c075706c7cf
topkoka <topkoka4000@gmail.com>
2019-04-02 18:05:47 +0700

    Python Types of Operators

---
+++ l1/GetArea.py
@@ -0,0 +1,9 @@
+import math 
+
+width = 50
+height = 25
+radius = 7
+areaCircle = (math.pi*(radius**2))
+
+print('area of width =',width,'and height = ',height,'radius =',radius)
+print('Area of Circle =',areaCircle)
