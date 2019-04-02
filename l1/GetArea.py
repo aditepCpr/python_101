2d8cd607db0716a3d1b11d5cfbc6bb6b03d0ceba
topkoka <topkoka4000@gmail.com>
2019-04-02 18:23:51 +0700

    test v2

--- l1/GetArea.py
+++
@@ -1,9 +0,0 @@
-import math 
-
-width = 50
-height = 25
-radius = 7
-areaCircle = (math.pi*(radius**2))
-
-print('area of width =',width,'and height = ',height,'radius =',radius)
-print('Area of Circle =',areaCircle)
