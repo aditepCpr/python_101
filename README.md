18bbf799d09a602fa27e33fa668a3e6faf7089fd
aditep <topkoka4000@gmail.com>
2019-04-04 16:01:09 +0700

    update raedme Logical

--- README.md
+++ README.md
@@ -31,7 +31,14 @@ elif a<5:
 
 ``` and,or,not ```
 ```python
-
+a=6
+b=5
+c= True
+if a>5 and b>4:
+    print('hi')
+elif a<5 or b<6:
+    print('low')   
+print(not c) # !True = False
 ```
 * Assignment
 
--- l2/tempCodeRunnerFile.py
+++ l2/tempCodeRunnerFile.py
@@ -1,8 +1,8 @@
-num = 3.4
-
-if num > 0:
-    print('Positive number')
-elif num == 0:
-    print('Zero')
-else:
-    print('Negative number')
+a=6
+b=5
+c= True
+if a>5 and b>4:
+    print('hi')
+elif a<5 or b<6:
+    print('low')   
+print(not c)
