d69817e8b01d77fc2e0d4cc28752ed52b3d468a8
topkoka <topkoka4000@gmail.com>
2019-04-10 00:41:39 +0700

    update by pycharm

--- l1_types_of_Operator/GetArea.py
+++ l1_types_of_Operator/GetArea.py
@@ -1,12 +1,12 @@
-import math 
+import math
 
 width = 50
 height = 25
 radius = 7
-areaCircle = (math.pi*(radius**2))
+areaCircle = (math.pi * (radius ** 2))
 
-print('area of width =',width,'and height = ',height,'radius =',radius)
-print('Area of Circle =',areaCircle)
+print('area of width =', width, 'and height = ', height, 'radius =', radius)
+print('Area of Circle =', areaCircle)
 
 # Output
 # area of width = 50 and height =  25 radius = 7
