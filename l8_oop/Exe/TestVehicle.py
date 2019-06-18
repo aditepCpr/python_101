fb6d1106ddfc8477608d9c846f8f493c4206cb02
topkoka <topkoka4000@gmail.com>
2019-06-19 04:18:31 +0700

    l8_oop lab

--- l8_oop/Exe/TestVehicle.py
+++ l8_oop/Exe/TestVehicle.py
@@ -1,4 +1,4 @@
-from l8_oop.Exe.Vehicle import Car
+from l8_oop.Exe.Vehicle import *
 
 honda = Car('red', 4, "IVTech")
 crv = Car('silver', 5, 'IVTech')
@@ -10,3 +10,30 @@ print(type(crv))
 honda.info()
 crv.info()
 toyota.info()
+print("-"*30)
+
+j1 = Jazz()
+j1.info()
+print("-"*30)
+print('CRV')
+crv = CRV('pink',4,'ivtech','Intelligence')
+crv.info()
+
+print("-"*30)
+
+# Inheritance
+audi = Car('Silver',4,'Super Engine')
+crv = CRV()
+jazz = Jazz('silver',4,'IVTech')
+
+owner = Owner('001','Tony Stark')
+
+# polymorphism
+owner.buyCar(audi)
+owner.buyCar(crv)
+owner.buyCar(jazz)
+# Encapsulation
+owner.getInfo()
+
+
+
