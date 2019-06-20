79032cb900b242414df02727743149feba132aaa
topkoka <topkoka4000@gmail.com>
2019-06-20 23:22:36 +0700

    l9_datetime  datetime and Calender :: LAB

--- l9_datetime/lab/Test.py
+++ l9_datetime/lab/Test.py
@@ -9,3 +9,21 @@ emp2 = Employee('E-02','top','1-DEC-1985','10-DEC-2000')
 emp2.info()
 
 print("#"*100)
+
+# ID : E-01
+# NAME : Aditep Campira
+# BD: 1-MAR-1995
+# StarWork : 1-DEC-2018
+# Age : 24 year
+# Age Of work : 6 month 21 day
+# vacation 10 day
+# ####################################################################################################
+# ID : E-02
+# NAME : top
+# BD: 1-DEC-1985
+# StarWork : 10-DEC-2000
+# Age : 33 year
+# 0.5369863013698613
+# Age Of work : 18 year 6 month 15 day
+# vacation 15 day
+# ####################################################################################################
